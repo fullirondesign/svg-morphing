143d65ecf18e4f38fc44ecf819a0547cee43774d
ilyafefelov <ilyafefelov@gmail.com>
2019-03-11 11:29:36 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './css/main.css'
+import anime from 'lib/anime.es.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+        <svg id="morph" width="100%" height="100%" viewBox="0 0 1920 1080" preserveAspectRatio="none" >
+          
+          <path id="morphFull" class="a" d="M.1,1080h1920L1919.978-.18.1,0Z" transform="translate(0.404 0.68)"/>
+        </svg>
+
+
       </div>
     );
   }
