a9b0a096d76522096f68bd6bef62944d726ef5e5
full_iron <ilyafefelov@gmail.com>
2019-03-14 15:47:38 +0200

    animation id in hook

--- src/App.js
+++ src/App.js
@@ -14,40 +14,55 @@ class App extends Component {
       preserveAspectRatio="none"
       // x="0px" y="0px"
     >
-      <path class="morph" d="M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z"/>
+      <path
+        class="morph"
+        d="M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z"
+      />
     </svg>
   );
 
   componentDidMount() {
-      // animation.restart()
+    // animation.restart()
   }
 
   handleClick(e) {
     e.preventDefault();
-    console.log ("click: ")
-    console.log (e)
+    console.log("click: ");
+    console.log(e);
 
     anime({
       targets: ".morph",
       d: [
-        { value: "M 1920 1080 L 1474.6 584 S 1137.53 679 969 679 c -168 0 -504 -471 -504 -471 S 155 1080 0 1080 V 0 H 1920 Z" },
-        { value: "M 1920 1080 L 1974.6 -200 S 1137.53 -200 969 -300 c -168 -100 -504 -471 -904 -471 S 15 0 0 1080 V 0 H 1920 Z" },
+        {
+          value:
+            "M 1920 1080 L 1474.6 584 S 1137.53 679 969 679 c -168 0 -504 -471 -504 -471 S 155 1080 0 1080 V 0 H 1920 Z"
+        },
+        {
+          value:
+            "M 1920 1080 L 1974.6 -200 S 1137.53 -200 969 -300 c -168 -100 -504 -471 -904 -471 S 15 0 0 1080 V 0 H 1920 Z"
+        }
         // { value: "M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z" }
       ],
       easing: "easeInQuad",
       duration: 2000,
       loop: false,
       autoplay: false
-    })
-    .play();
+    }).play();
   }
 
   render() {
-    
-    return (<div className="App">
-        <BgSVGanime />
-    </div>);
+    return (
+      <div className="App">
+        <div className="animations">
+              <div className="animationBox">
+                    <div className="animation">
+                      <BgSVGanime animationId={"morph"}/>
+                    </div>
+              </div>
+        </div> 
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/animations/background-svg-anime.js
+++ src/components/animations/background-svg-anime.js
@@ -4,24 +4,43 @@ import React from "react";
 import anime from "animejs"; // animation library
 
 //  SVG itself ----
-const svg = (
-  <svg
-    id="morph"
-    width="auto"
-    height="auto"
-    viewBox="0 0 1920 1080"
-    preserveAspectRatio="none"
-    // x="0px" y="0px"
-  >
-    <path class="morph" d="M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z"/>
-  </svg>
-);
+const svg = (animationId) => {
+  return(
+    <svg
+      id={`${animationId}`}
+      width="auto"
+      height="auto"
+      viewBox="0 0 1920 1080"
+      preserveAspectRatio="none"
+      // x="0px" y="0px"
+    >
+      <path class={`${animationId}`} d="M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z"/>
+    </svg>
+    
+  )
+};
+
+
+
+
+//   <svg
+//     id={animationId}
+//     width="auto"
+//     height="auto"
+//     viewBox="0 0 1920 1080"
+//     preserveAspectRatio="none"
+//     // x="0px" y="0px"
+//   >
+//     <path class={animationId} d="M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z"/>
+//   </svg>
+// );
 
 // morphing on Click function ----
-const handleClick = () => {   
-  console.log ("click")
+const handleClick = (animationId) => {   
+  console.log (`click on ${animationId}`)
+  console.log (svg(animationId))
   anime({
-      targets: ".morph",
+      targets: `.${animationId}`,
       d: [
       { value: "M 1920 1080 L 1474.6 584 S 1137.53 679 969 679 c -168 0 -504 -471 -504 -471 S 155 1080 0 1080 V 0 H 1920 Z" },
       { value: "M 1920 1080 L 1974.6 -200 S 1137.53 -200 969 -300 c -168 -100 -504 -471 -904 -471 S 15 0 0 1080 V 0 H 1920 Z" },
@@ -36,10 +55,11 @@ const handleClick = () => {
 }
 
 // hook ----
-const BgSVGanime = ({ children = {} }) => (
+const BgSVGanime = ({ children, props, animationId = {} }) => (
     <div className="animation">
-        {svg}
-        <div className="butt" onClick={handleClick}>
+        {props}
+        {/* {(animationId)=>svg(animationId)} */}
+        <div className="butt" onClick={handleClick(animationId)}>
             go
         </div>
     </div>
