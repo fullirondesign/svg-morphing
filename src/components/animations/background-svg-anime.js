f75a6d7b7cfdb083fa591d5c3fb13ce23cafaf8e
full_iron <ilyafefelov@gmail.com>
2019-03-15 14:18:47 +0200

    state added

--- src/components/animations/background-svg-anime.js
+++ src/components/animations/background-svg-anime.js
@@ -1,11 +1,11 @@
 // HOOK animation for SVG background morphing
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import anime from "animejs"; // animation library
 
 //  SVG itself ----
-const svg = (animationId) => {
-  return(
+const svg = animationId => {
+  return (
     <svg
       id={`${animationId}`}
       width="auto"
@@ -14,41 +14,72 @@ const svg = (animationId) => {
       preserveAspectRatio="none"
       // x="0px" y="0px"
     >
-      <path class={`${animationId}`} d="M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z"/>
+      <path
+        class={`${animationId}`}
+        d="M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z"
+      />
     </svg>
-    
-  )
+  );
 };
 
+// hook ----
+const BgSVGanime = ({ children, animationId }) => {
+  // state
+  const [animationStage, setAnimationStage] = useState("start");
 
-// morphing onClick function ----
-const handleClick = (animationId) => {   
-  console.log (`click on ${animationId}`)
-  console.log ("svg: ",svg(animationId))
-  anime({
+  // morphing onClick function ----
+  const handleClick = (
+    animationId,
+    
+    
+  ) => {
+    console.log(`click on ${animationId}`);
+    console.log("svg: ", svg(animationId));
+    console.log("animationStage: ", animationStage);
+
+    if (animationStage === "start") {
+      setAnimationStage("end");
+      console.log(
+        `animation stage: ${animationStage}, animation id: ${animationId}`
+      );
+    } else
+    if (animationStage === "end") {
+      setAnimationStage("start");
+      console.log(`animation error ${animationId}`);
+    } else {
+      console.log(`animation error ${animationId}`);
+    }
+
+    anime({
       targets: `.${animationId}`,
       d: [
-      { value: "M 1920 1080 L 1474.6 584 S 1137.53 679 969 679 c -168 0 -504 -471 -504 -471 S 155 1080 0 1080 V 0 H 1920 Z" },
-      { value: "M 1920 1080 L 1974.6 -200 S 1137.53 -200 969 -300 c -168 -100 -504 -471 -904 -471 S 15 0 0 1080 V 0 H 1920 Z" },
-      // { value: "M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z" }
+        {
+          value:
+            "M 1920 1080 L 1474.6 584 S 1137.53 679 969 679 c -168 0 -504 -471 -504 -471 S 155 1080 0 1080 V 0 H 1920 Z"
+        },
+        {
+          value:
+            "M 1920 1080 L 1974.6 -200 S 1137.53 -200 969 -300 c -168 -100 -504 -471 -904 -471 S 15 0 0 1080 V 0 H 1920 Z"
+        }
+        // { value: "M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z" }
       ],
       easing: "easeInQuad",
       duration: 2000,
       loop: false,
       autoplay: false
-  })
-  .play()
-  console.log ("anime result", anime());
-}
+    }).play();
+    console.log("anime result", anime());
+  };
 
-// hook ----
-const BgSVGanime = ({ children, animationId = {} }) => (
+  return (
     <div className="animation">
-        {svg(animationId)}
-        <div className="butt" onClick={() => handleClick(animationId)}>
-            go
-        </div>
+      {svg(animationId)}
+      {animationStage}
+      <div className="butt" onClick={() => handleClick(animationId)}>
+        go
+      </div>
     </div>
-)
+  );
+};
 
-export default BgSVGanime
+export default BgSVGanime;
