c4245f8a0be6fdcda6eb541a0d00446f1f14651e
full_iron <ilyafefelov@gmail.com>
2019-03-15 15:13:55 +0200

    formating

--- src/components/animations/background-svg-anime.js
+++ src/components/animations/background-svg-anime.js
@@ -34,18 +34,22 @@ const BgSVGanime = ({ children, animationId }) => {
     console.log("animationStage: ", animationStage);
 
     let animationFrame, animationCurrentFrame1, animationCurrentFrame2;
-    
+
     let frames = {
       0: "M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z",
       1: "M 1920 1080 L 1474.6 584 S 1137.53 679 969 679 c -168 0 -504 -471 -504 -471 S 155 1080 0 1080 V 0 H 1920 Z",
       2: "M 1920 1080 L 1974.6 -200 S 1137.53 -200 969 -300 c -168 -100 -504 -471 -904 -471 S 15 0 0 1080 V 0 H 1920 Z"
     };
-    
-    const{0:animationFrame0, 1:animationFrame1, 2:animationFrame2} = frames;
-    console.log('frames', frames)
-    console.log("animationFrame0 :", animationFrame1)
-    console.log("animationFrame1 :", animationFrame1)
-    console.log("animationFrame2 :", animationFrame2)
+
+    const {
+      0: animationFrame0,
+      1: animationFrame1,
+      2: animationFrame2
+    } = frames;
+    console.log("frames", frames);
+    console.log("animationFrame0 :", animationFrame1);
+    console.log("animationFrame1 :", animationFrame1);
+    console.log("animationFrame2 :", animationFrame2);
     if (animationStage === "start") {
       setAnimationStage("end");
       console.log(
