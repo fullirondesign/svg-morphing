3fbc2e8b13e5dff7823cdc93dd24b834527a6050
full_iron <ilyafefelov@gmail.com>
2019-03-15 16:00:49 +0200

    router added

--- src/App.js
+++ src/App.js
@@ -4,68 +4,38 @@ import "./css/main.css";
 import anime from "animejs";
 import BgSVGanime from "./components/animations/background-svg-anime";
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 class App extends Component {
-  svg = (
-    <svg
-      id="morph"
-      width="auto"
-      height="auto"
-      viewBox="0 0 1920 1080"
-      preserveAspectRatio="none"
-      // x="0px" y="0px"
-    >
-      <path
-        class="morph"
-        d="M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z"
-      />
-    </svg>
-  );
-
-  componentDidMount() {
-    // animation.restart()
-  }
-
-  handleClick(e) {
-    e.preventDefault();
-    console.log("click: ");
-    console.log(e);
-
-    anime({
-      targets: ".morph",
-      d: [
-        {
-          value:
-            "M 1920 1080 L 1474.6 584 S 1137.53 679 969 679 c -168 0 -504 -471 -504 -471 S 155 1080 0 1080 V 0 H 1920 Z"
-        },
-        {
-          value:
-            "M 1920 1080 L 1974.6 -200 S 1137.53 -200 969 -300 c -168 -100 -504 -471 -904 -471 S 15 0 0 1080 V 0 H 1920 Z"
-        }
-        // { value: "M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z" }
-      ],
-      easing: "easeInQuad",
-      duration: 2000,
-      loop: false,
-      autoplay: false
-    }).play();
-  }
-
   render() {
-    return (
-      <div className="App">
-        <div className="animations">
-              <div className="animationBox">
-                    <div className="animation">
-                      <BgSVGanime animationId={"morph"}/>
-                    </div>
-              </div>
-              <div className="animationBox">
-                    <div className="animation">
-                      <BgSVGanime animationId={"lis"}/>
-                    </div>
-              </div>
-        </div> 
+    const Home = () => (
+      <div className="animations">
+        <div className="animationBox">
+        <div className="animationName">animation 1</div>
+          <div className="animation">
+            <BgSVGanime animationId={"morph"} />
+          </div>
+        </div>
+        <div className="animationBox">
+        <div className="animationName">animation 2</div>
+          <div className="animation">
+            <BgSVGanime animationId={"lis"} />
+          </div>
+        </div>
       </div>
+    );
+
+
+    return (
+      <Router>
+        <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={Home} />
+        <Route path="/topic" component={Home} />
+        <Route component={()=>"404"} />
+        </div>
+        
+      </Router>
     );
   }
 }
--- src/components/animations/background-svg-anime.js
+++ src/components/animations/background-svg-anime.js
@@ -22,25 +22,26 @@ const svg = animationId => {
   );
 };
 
-// hook ----
+// Hook ----
 const BgSVGanime = ({ children, animationId }) => {
-  // state
+  // State
   const [animationStage, setAnimationStage] = useState("start");
-
-  // morphing onClick function ----
+  ////////////////////////////////////////////
+  // Animation. Morphing onClick function ----
   const handleClick = animationId => {
+    // check
     console.log(`click on ${animationId}`);
     console.log("svg: ", svg(animationId));
     console.log("animationStage: ", animationStage);
 
-    let animationFrame, animationCurrentFrame1, animationCurrentFrame2;
-
+    // frames
     let frames = {
       0: "M 1920 1080 L 1474.6 1080 S 1137.53 1080 969 1080 c -168 000 -2000 220 -304 -471 S 155 1080 0 1080 V 0 H 1920 Z",
       1: "M 1920 1080 L 1474.6 584 S 1137.53 679 969 679 c -168 0 -504 -471 -504 -471 S 155 1080 0 1080 V 0 H 1920 Z",
       2: "M 1920 1080 L 1974.6 -200 S 1137.53 -200 969 -300 c -168 -100 -504 -471 -904 -471 S 15 0 0 1080 V 0 H 1920 Z"
     };
 
+    // Keys for present, current, manipulated animations' key-frames
     const {
       0: animationFrame0,
       1: animationFrame1,
@@ -50,6 +51,10 @@ const BgSVGanime = ({ children, animationId }) => {
     console.log("animationFrame0 :", animationFrame1);
     console.log("animationFrame1 :", animationFrame1);
     console.log("animationFrame2 :", animationFrame2);
+
+    // animation control
+    // -- stage-depended frames config
+    let animationCurrentFrame1, animationCurrentFrame2; // for current -manipulated- timeline
     if (animationStage === "start") {
       setAnimationStage("end");
       console.log(
@@ -67,8 +72,9 @@ const BgSVGanime = ({ children, animationId }) => {
     } else {
       console.log(`animation error ${animationId}`);
     }
-
+    // animation function
     anime({
+      // <----config
       targets: `.${animationId}`,
       d: [
         {
@@ -82,17 +88,18 @@ const BgSVGanime = ({ children, animationId }) => {
       duration: 2000,
       loop: false,
       autoplay: false
-    }).play();
+    }).play(); // <----action
+    ////////////////////////////////////////////
     console.log("anime result", anime());
   };
 
   return (
     <div className="animation">
-      {svg(animationId)}
-      {animationStage}
+      {" "}
+      {svg(animationId)} {animationStage}{" "}
       <div className="butt" onClick={() => handleClick(animationId)}>
-        go
-      </div>
+        go{" "}
+      </div>{" "}
     </div>
   );
 };
